de5c0ae35bae74ce0b9d863e01d774b288e1c7d7
Drew Lirette <drewlirette@cox.net>
2026-08-29 11:36:27 -0500

    Add salon, Instagram, Facebook, and Google links to contact page

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,8 @@ export const SITE = {
   description:
     'Amber Uncut: one beginner learning about grooming, confidence, and life one note at a time.',
   author: 'Amber',
+  salon: 'https://www.cutsbyamber.com',
   instagram: 'https://www.instagram.com/cuts_by_amber/',
+  facebook: 'https://www.facebook.com/profile.php?id=61566664180427',
+  google: 'https://share.google/62bNcQEDrT8RAzOY0',
 };
